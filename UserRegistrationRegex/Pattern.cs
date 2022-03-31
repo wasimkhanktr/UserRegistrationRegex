34c3979a01b99cde0ccfea477d690a9f36b60917
MdWasimAkram <wasimkhanktr@gmail.com>
2022-03-31 22:19:37 +0530

    [Wasim] Add: create your First Name

---
+++ UserRegistrationRegex/Pattern.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistrationRegex
+{
+    internal class Pattern
+    {
+        public string FirstName = "^[A-Z]{1}[a-z]{2,}$";
+        public void validate(string firstName)
+        {
+            bool correct = Regex.IsMatch(firstName, FirstName);
+            if (correct)
+                Console.WriteLine("FirstName is correct");
+            else
+                Console.WriteLine("FirstName is not correct");
+        }
+    }
+}
---
+++ UserRegistrationRegex/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserRegistrationRegex
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pattern pattern = new Pattern();
+            Console.Write("Enter the FirstName : ");
+            string firstName = Console.ReadLine();
+            pattern.validate(firstName);
+        }
+    }
+}
